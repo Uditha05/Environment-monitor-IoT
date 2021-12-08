5099fd911eb0af5638505bb6f588ff272dc0ef43
Uditha <ui25.isuranga@gmail.com>
2021-12-08 17:11:20 +0530

    init project

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask,request,jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello!</h1>"
+
+@app.route('/json-example', methods=['POST'])
+def json_example():
+    request_data = request.get_json()
+
+    language = request_data['language']
+    framework = request_data['framework']
+
+    # two keys are needed because of the nested object
+    python_version = request_data['version_info']['python']
+
+    # an index is needed because of the array
+    example = request_data['examples'][0]
+
+    boolean_test = request_data['boolean_test']
+
+    return '''
+           The language value is: {}
+           The framework value is: {}
+           The Python version is: {}
+           The item at index 0 in the example list is: {}
+           The boolean value is: {}'''.format(language, framework, python_version, example, boolean_test)
+
+
+@app.route('/jsonexample', methods=['GET'])
+def getValues():
+    d = {"aa": 23}
+    return jsonify(d)
